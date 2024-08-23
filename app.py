92de6b05f2d09f700131a92b26065f37d3d4e93b
Jimmy Murillo <salchichongallo1997@gmail.com>
2024-08-22 21:55:40 -0500

    ci: create service

--- app.py
+++ app.py
@@ -5,6 +5,3 @@ app = Flask(__name__)
 @app.route("/")
 def helloworld():
      return "Hello World!"
-
-if __name__ == '__main__':
-     app.run(host='0.0.0.0', port='3001', debug=False)
---
+++ scripts/create-service.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+app="$APP_NAME"
+port="$APP_PORT"
+
+app_dir="/var/www/$app"
+
+$service_file="/etc/systemd/system/$app.service"
+
+$current_user=$(whoami)
+
+sudo cat <<EOF > $service_file
+[Unit]
+Description=$app App
+After=network.target
+
+[Service]
+ExecStart=$app_dir/venv/bin/python -m flask run -h 0.0.0.0 -p $port
+WorkingDirectory=$app_dir
+Restart=always
+User=$current_user
+
+[Install]
+WantedBy=multi-user.target
+EOF
