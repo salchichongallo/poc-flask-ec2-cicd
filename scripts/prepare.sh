82b88843d7bfaa8ea84b400a72b6cc7de3f1814d
Jimmy Murillo <salchichongallo1997@gmail.com>
2024-08-22 21:20:34 -0500

    ci: add sudo to scripts

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -4,9 +4,9 @@ app="$APP_NAME"
 
 if systemctl is-active --quiet $app; then
     echo "Stopping $app..."
-    systemctl stop $app
-    systemctl disable $app
-    rm /etc/systemd/system/$app.service
+    sudo systemctl stop $app
+    sudo systemctl disable $app
+    sudo rm /etc/systemd/system/$app.service
     echo "$app stopped."
 else
     echo "$app is not running."
@@ -14,10 +14,8 @@ fi
 
 if [ -d /var/www/$app ]; then
     echo "Removing /var/www/$app..."
-    rm -rf /var/www/$app
+    sudo rm -rf /var/www/$app
     echo "/var/www/$app removed."
 else
     echo "/var/www/$app does not exist."
 fi
-
-echo "$app was removed." > ~/deploy-status.txt
